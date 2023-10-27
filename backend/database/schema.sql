8772c430fe558f0ebd8d734c97142489b5da4a4a
Ryanj24 <57965902+Ryanj24@users.noreply.github.com>
2023-10-27 09:00:06 +0100

    Changes made to bank transaction procedure to include a transaction and error handling

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE users (
 CREATE TABLE bank_accounts (
     `id` INT unsigned NOT NULL AUTO_INCREMENT,
     `account_name` VARCHAR(20),
-    `balance` DECIMAL(10, 2),
+    `balance` DECIMAL(10, 2) CHECK(`balance` >= 0),
     `account_owner_id` INT unsigned,
     PRIMARY KEY(`id`),
     FOREIGN KEY(`account_owner_id`) REFERENCES users(`id`)
@@ -20,6 +20,7 @@ CREATE TABLE bank_accounts (
 CREATE TABLE stock_portfolio (
     `id` INT unsigned NOT NULL AUTO_INCREMENT,
     `portfolio_name` VARCHAR(20),
+    `balance` DECIMAL(10, 2),
     `portfolio_owner_id` INT unsigned,
     PRIMARY KEY(`id`),
     FOREIGN KEY(`portfolio_owner_id`) REFERENCES `users`(`id`)
@@ -67,19 +68,43 @@ CREATE PROCEDURE bank_transaction_procedure(
     IN category VARCHAR(20),
     IN transaction_date DATE)
 BEGIN
-    IF transaction_type = "Income" OR transaction_type = "Deposit" THEN
-        UPDATE `bank_accounts` SET `balance` = `balance` + amount
-        WHERE `account_id` = account_id;
 
-        INSERT INTO `bank_transactions` (account_id, type, category, transaction_date, amount)
-        VALUES (account_id, transaction_type, category, transaction_date, amount);
-    ELSE
-        UPDATE `bank_accounts` SET `balance` = `balance` - amount
-        WHERE `account_id` = account_id;
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        ROLLBACK;
+    END;
 
-        INSERT INTO `bank_transactions` (account_id, type, category, transaction_date, amount)
-        VALUES (account_id, transaction_type, category, transaction_date, amount);
-    END IF;
+    START TRANSACTION;
+        IF transaction_type = "Income" OR transaction_type = "Deposit" THEN
+            UPDATE `bank_accounts` SET `balance` = `balance` + amount
+            WHERE `account_id` = account_id;
+
+            INSERT INTO `bank_transactions` (account_id, type, category, transaction_date, amount)
+            VALUES (account_id, transaction_type, category, transaction_date, amount);
+        ELSE
+            UPDATE `bank_accounts` SET `balance` = `balance` - amount
+            WHERE `account_id` = account_id;
+
+            INSERT INTO `bank_transactions` (account_id, type, category, transaction_date, amount)
+            VALUES (account_id, transaction_type, category, transaction_date, amount);
+        END IF;
+    COMMIT;
 END//
 DELIMITER ;
 
+-- DELIMITER //
+-- CREATE PROCEDURE stock_transaction_procedure(
+--     IN portfolio_id INT,
+--     IN portfolio_owner_id INT,
+--     IN transaction_type ENUM("Buy", "Sell"),
+--     IN company_ticker VARCHAR(4),
+--     IN price_per_share DECIMAL(8, 2),
+--     IN quantity INT,
+--     IN transaction_date DATE
+-- )
+-- BEGIN
+--     IF transaction_type = "Buy" THEN
+
+--     END IF;
+-- END//
+-- DELIMITER ;
