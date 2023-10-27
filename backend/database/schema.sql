4c475b96edf5d5eb28c15ddcc5ef4b69c0105920
Ryanj24 <57965902+Ryanj24@users.noreply.github.com>
2023-10-27 09:25:35 +0100

    Fixes made to bank transactions procedure

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -20,7 +20,7 @@ CREATE TABLE bank_accounts (
 CREATE TABLE stock_portfolio (
     `id` INT unsigned NOT NULL AUTO_INCREMENT,
     `portfolio_name` VARCHAR(20),
-    `balance` DECIMAL(10, 2),
+    `balance` DECIMAL(10, 2) CHECK(`balance` >= 0),
     `portfolio_owner_id` INT unsigned,
     PRIMARY KEY(`id`),
     FOREIGN KEY(`portfolio_owner_id`) REFERENCES `users`(`id`)
@@ -77,6 +77,40 @@ BEGIN
     START TRANSACTION;
         IF transaction_type = "Income" OR transaction_type = "Deposit" THEN
             UPDATE `bank_accounts` SET `balance` = `balance` + amount
+            WHERE `id` = `account_id` AND `account_owner_id` = account_owner_id;
+
+            INSERT INTO `bank_transactions` (account_id, type, category, transaction_date, amount)
+            VALUES (account_id, transaction_type, category, transaction_date, amount);
+        ELSE
+            UPDATE `bank_accounts` SET `balance` = `balance` - amount
+            WHERE `id` = `account_id` AND `account_owner_id` = account_owner_id;
+
+            INSERT INTO `bank_transactions` (account_id, type, category, transaction_date, amount)
+            VALUES (account_id, transaction_type, category, transaction_date, amount);
+        END IF;
+    COMMIT;
+END//
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE stock_transaction_procedure(
+    IN portfolio_id INT, 
+    IN company_ticker VARCHAR(4),
+    IN transaction_type ENUM("Buy", "Sell"),
+    IN transaction_date DATE,
+    IN quantity INT,
+    IN price_per_share DECIMAL(8, 2)
+    )
+BEGIN
+
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        ROLLBACK;
+    END;
+
+    START TRANSACTION;
+        IF transaction_type = "Buy" THEN
+            UPDATE `stock_portfolio` SET `balance` = `balance` - amount
             WHERE `account_id` = account_id;
 
             INSERT INTO `bank_transactions` (account_id, type, category, transaction_date, amount)
@@ -91,20 +125,3 @@ BEGIN
     COMMIT;
 END//
 DELIMITER ;
-
--- DELIMITER //
--- CREATE PROCEDURE stock_transaction_procedure(
---     IN portfolio_id INT,
---     IN portfolio_owner_id INT,
---     IN transaction_type ENUM("Buy", "Sell"),
---     IN company_ticker VARCHAR(4),
---     IN price_per_share DECIMAL(8, 2),
---     IN quantity INT,
---     IN transaction_date DATE
--- )
--- BEGIN
---     IF transaction_type = "Buy" THEN
-
---     END IF;
--- END//
--- DELIMITER ;
