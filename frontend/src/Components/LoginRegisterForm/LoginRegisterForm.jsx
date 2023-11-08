13de053487db8cfd500ccf021231a99c994ea527
Ryanj24 <57965902+Ryanj24@users.noreply.github.com>
2023-11-08 09:43:45 +0000

    Added react-hook-form to control form state and form submission

--- frontend/src/Components/LoginRegisterForm/LoginRegisterForm.jsx
+++ frontend/src/Components/LoginRegisterForm/LoginRegisterForm.jsx
@@ -2,42 +2,50 @@ import React from 'react'
 import './LoginRegisterForm.css'
 import { Box, Button, Container, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form' 
 
 const LoginRegisterForm = ({formType}) => {
+
+  const {register, handleSubmit} = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+
   return (
     <Container component="div" className="form-layout-container" sx={{display: "flex", flexDirection: "column", alignItems: "center"}} maxWidth="sm">
 
         {formType === "Register"
         ?
           <>
-            <form id='register-form'>
+            <form id='register-form' onSubmit={handleSubmit(onSubmit)}>
               <Box component="div" className='form-inputs'>
                 <div className="first-name-input">
                   <label htmlFor='first_name'>First Name</label>
-                  <input type="text" id='first_name' name='first_name'/>
+                  <input type="text" id='first_name' {...register("first_name")}/>
                 </div>
 
                 <div className="last-name-input">
                   <label htmlFor='last_name'>Last Name</label>
-                  <input type="text" id='last_name' name='last_name'/>
+                  <input type="text" id='last_name' {...register("last_name")}/>
                 </div>
 
                 <div className="email-input">
                   <label htmlFor='email'>Email</label>
-                  <input type="email" id='email' name='email'/>
+                  <input type="email" id='email' {...register("email")}/>
                 </div>
 
                 <div className="password-input">
                   <label htmlFor='password'>Password</label>
-                  <input type="password" id='password' name='password'/>
+                  <input type="password" id='password' {...register("password")}/>
                 </div>
 
                 <div className="dob-input">
                   <label htmlFor='dob'>Date of Birth</label>
-                  <input type="date" id='dob' name='dob'/>
+                  <input type="date" id='dob' {...register("dob")}/>
                 </div>
               </Box>
-              <Button variant='contained' sx={{textTransform: "none", borderRadius: "10px"}}>Create Account</Button>
+              <Button variant='contained' sx={{textTransform: "none", borderRadius: "10px"}} type='submit'>Create Account</Button>
             </form>
             <Typography variant='body2' component="p" className='login-register-link'>
               Already have an account? <Link to={"/login"} style={{color: "var(--primary-col)"}}>Log in</Link>
@@ -45,19 +53,19 @@ const LoginRegisterForm = ({formType}) => {
           </>
         :
           <>
-            <form id='login-form'>
+            <form id='login-form' onSubmit={handleSubmit(onSubmit)}>
               <Box component="div" className='form-inputs'>
                 <div className="email-input">
                   <label htmlFor='email'>Email</label>
-                  <input type="email" id='email' name='email'/>
+                  <input type="email" id='email' {...register("email")}/>
                 </div>
 
                 <div className="password-input">
                   <label htmlFor='password'>Password</label>
-                  <input type="password" id='password' name='password'/>
+                  <input type="password" id='password' {...register("password")}/>
                 </div>
               </Box>
-              <Button variant='contained' sx={{textTransform: "none", borderRadius: "10px"}}>Login</Button>
+              <Button variant='contained' sx={{textTransform: "none", borderRadius: "10px"}} type='submit'>Login</Button>
             </form>
             <Typography variant='body2' component="p" className='login-register-link'>
               New to myFinance? <Link to={"/register"} style={{color: "var(--primary-col)"}}>Create an account</Link>
