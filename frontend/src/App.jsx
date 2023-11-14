f1a875b4afc859e58eecc94f1554e351a8cecf41
Ryanj24 <57965902+Ryanj24@users.noreply.github.com>
2023-11-14 09:51:24 +0000

    Added page components for profile, accounts and portfolio along with their routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,10 @@ import LandingPage from './Pages/LandingPage/LandingPage.jsx'
 import Register from './Pages/Register/Register.jsx'
 import Login from './Pages/Login/Login.jsx'
 import Dashboard from './Pages/Dashboard/Dashboard.jsx'
+import Profile from './Pages/Profile/Profile.jsx'
+import Accounts from './Pages/Accounts/Accounts.jsx'
+import Portfolio from './Pages/Portfolio/Portfolio.jsx'
+import Settings from './Pages/Settings/Settings.jsx'
 import MainLayout from './Components/MainLayout/MainLayout.jsx'
 
 function App() {
@@ -13,17 +17,13 @@ function App() {
           <Route path="/landing" element={<LandingPage />}/>
           <Route path="/login" element={<Login />}/>
           <Route path="/register" element={<Register />}/>
-          {/* <Route path="/dashboard" element={<Dashboard />}/> */}
           <Route path='home' element={<MainLayout />}>
             <Route path="dashboard" element={<Dashboard />}/>
+            <Route path="profile" element={<Profile />}/>
+            <Route path="accounts" element={<Accounts />}/>
+            <Route path="portfolio" element={<Portfolio />}/>
+            <Route path="settings" element={<Settings />}/>
           </Route>
-
-          {/* <Route path="/dashboard" element={<Dashboard />}/>
-          <Route path="/profile" element={<UserProfile />}/>
-          <Route path="/accounts" element={<Accounts />}/>
-          <Route path="/stockportfolios" element={<Portfolio />}/>
-          <Route path="/login" element={<Login />}/>
-          <Route path="/register" element={<Register />}/> */}
         </Routes>
       </BrowserRouter>
     </>
