c0f795eef1b79e552482b2e87832218058ee3eac
Ryanj24 <57965902+Ryanj24@users.noreply.github.com>
2023-11-23 15:55:56 +0000

    Fixes made to goals table dashboard card styling

--- frontend/src/Components/DashboardCards/Goals.jsx
+++ frontend/src/Components/DashboardCards/Goals.jsx
@@ -1,7 +1,6 @@
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import { useSelector } from 'react-redux'
@@ -15,10 +14,13 @@ const Goals = () => {
     return (
       <h1>Loading...</h1>
     )
-  } 
+  }
 
   return (
     <div className="goals">
+      <header className='goals-header'>
+          <h3>Goals</h3>
+      </header>
       <Table sx={{ maxWidth: "100%" }} aria-label="goal table" className='goal-table'>
         <TableHead>
           <TableRow>
@@ -32,7 +34,6 @@ const Goals = () => {
               <TableCell component="th" scope="row">
                 {goal.goal_name}
               </TableCell>
-              {/* <TableCell align="right">{goal.current_progress / goal.end_goal * 100}%</TableCell> */}
               <TableCell align="right">{roundNumbers(goal.current_progress / goal.end_goal, 3) * 100}%</TableCell>
             </TableRow>
           ))}
