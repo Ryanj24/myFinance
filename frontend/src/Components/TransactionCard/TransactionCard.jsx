be7c0cfb2ae53aab840ef46718addd4f71ceeb59
Ryanj24 <57965902+Ryanj24@users.noreply.github.com>
2023-12-17 08:51:29 +0000

    Added dropdown for each transaction card with options to edit or delete the transaction

--- frontend/src/Components/TransactionCard/TransactionCard.jsx
+++ frontend/src/Components/TransactionCard/TransactionCard.jsx
@@ -1,15 +1,25 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import './TransactionCard.css'
 import { Paper } from '@mui/material'
-import { MoreHoriz } from '@mui/icons-material'
+import { Edit, Delete, MoreHoriz } from '@mui/icons-material'
 import { dateFormatter } from '../../utilityFunctions/dateFormatter'
 
 const TransactionCard = ({id, icon, desc, date, amount, toggleModal, setCurrentTransactionID}) => {
 
+    const [dropdownActive, setDropdownActive] = useState(true);
 
     const handleOnClick = (e) => {
+        setDropdownActive(!dropdownActive)
+    }
+
+    const handleEditClick = () => {
+        setDropdownActive(false);
         setCurrentTransactionID(id)
         toggleModal(true)
+    }
+
+    const handleDeleteClick = () => {
+        console.log("Delete")
     }
   return (
     <Paper elevation={2} variant='elevation' className='transaction-card'>
@@ -25,11 +35,21 @@ const TransactionCard = ({id, icon, desc, date, amount, toggleModal, setCurrentT
         <div className="transaction-amount">
             {amount}
         </div>
-        <div className="transaction-action-btns">
+        <div className="transaction-options">
             <button onClick={handleOnClick}>
                 <MoreHoriz sx={{rotate: "90deg"}} />
             </button>
         </div>
+        {dropdownActive && 
+            <div className="transaction-action-btns">
+                <button onClick={handleEditClick}>
+                    <Edit /> Edit Transaction
+                </button>
+                <button onClick={handleDeleteClick}>
+                    <Delete /> Delete Transaction
+                </button>
+            </div>
+        }
     </Paper>
   )
 }
