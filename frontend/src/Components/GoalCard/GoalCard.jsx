5d1dba76cee2e15ca1777d2723d85e6853ea025d
Ryanj24 <57965902+Ryanj24@users.noreply.github.com>
2024-01-16 08:38:46 +0000

    Added textarea to goals form for goal description and a date field for the goal end date. Also added styling for the goal card to be expanded to show more details

--- frontend/src/Components/GoalCard/GoalCard.jsx
+++ frontend/src/Components/GoalCard/GoalCard.jsx
@@ -4,12 +4,18 @@ import { Link } from 'react-router-dom'
 import { Typography } from '@mui/material'
 import { ExpandMore } from '@mui/icons-material'
 import ProgressBar from './ProgressBar/ProgressBar'
+import {dateFormatter} from '../../utilityFunctions/dateFormatter.js'
 
 const GoalCard = ({name, desc, currentProg, endGoal, endDate, status}) => {
 
     const [expanded, setExpanded] = useState(false)
 
     const cardRef = useRef(null)
+
+    const currentDate = new Date()
+    const goalEndDate = new Date(endDate)
+    const remainingDays = (goalEndDate.getTime() - currentDate.getTime()) / (1000 * 60 * 60 * 24)
+
 
     useEffect(() => {
         if (expanded) {
@@ -19,21 +25,50 @@ const GoalCard = ({name, desc, currentProg, endGoal, endDate, status}) => {
         }
       }, [expanded])
     return (
-        <div className='goal-card' ref={cardRef}>
-            <div className="goal-name">
-                <Typography variant='h5' component="h5">{name}</Typography>
+        <>
+        {expanded
+        ?
+            <div className='goal-card expanded' ref={cardRef}>
+                <div className="goal-name">
+                    <Typography variant='h5' component="h5">{name}</Typography>
+                </div>
+                <div className="goal-progress">
+                    <ProgressBar currentProg={currentProg} endGoal={endGoal}/>
+                    <p className='progress-start'>{Intl.NumberFormat("en", {style: "currency", currency: "GBP"}).format(0)}</p>
+                    <p className='progress-end'>{Intl.NumberFormat("en", {style: "currency", currency: "GBP"}).format(endGoal)}</p>
+                </div>
+                <div className="goal-options">
+                    <button onClick={() => setExpanded(!expanded)}>
+                        <ExpandMore sx={{rotate: "180deg"}}/>
+                    </button>
+                </div>
+                <div className="goal-date">
+                    <Typography variant='h5' component="h5" gutterBottom>Achieve By</Typography>
+                    <Typography variant='h5' component="h5" sx={{paddingLeft: "20px"}}>{dateFormatter(endDate, "full date")} (Days remaining: {remainingDays > 0 ? Math.round(remainingDays) : 0})</Typography>
+                </div>
+                <div className="goal-description">
+                    <Typography variant='h5' component="h5" gutterBottom>Description</Typography>
+                    <Typography variant='body1' component="p" sx={{paddingLeft: "20px", fontSize: "20px"}}>{desc}</Typography>
+                </div>
             </div>
-            <div className="goal-progress">
-                <ProgressBar currentProg={currentProg} endGoal={endGoal}/>
-                <p className='progress-start'>{Intl.NumberFormat("en", {style: "currency", currency: "GBP"}).format(0)}</p>
-                <p className='progress-end'>{Intl.NumberFormat("en", {style: "currency", currency: "GBP"}).format(endGoal)}</p>
+        :
+            <div className='goal-card' ref={cardRef}>
+                <div className="goal-name">
+                    <Typography variant='h5' component="h5">{name}</Typography>
+                </div>
+                <div className="goal-progress">
+                    <ProgressBar currentProg={currentProg} endGoal={endGoal}/>
+                    <p className='progress-start'>{Intl.NumberFormat("en", {style: "currency", currency: "GBP"}).format(0)}</p>
+                    <p className='progress-end'>{Intl.NumberFormat("en", {style: "currency", currency: "GBP"}).format(endGoal)}</p>
+                </div>
+                <div className="goal-options">
+                    <button onClick={() => setExpanded(!expanded)}>
+                        <ExpandMore />
+                    </button>
+                </div>
             </div>
-            <div className="goal-options">
-                <button onClick={() => setExpanded(!expanded)}>
-                    <ExpandMore />
-                </button>
-            </div>
-        </div>
+        }
+        </>
     )
 }
 
--- frontend/src/Components/GoalForm/GoalForm.jsx
+++ frontend/src/Components/GoalForm/GoalForm.jsx
@@ -15,17 +15,22 @@ const GoalForm = ({formType, toggleModal}) => {
   return (
     <form id='goal-modal-form' onSubmit={handleSubmit(onSubmit)}>
         <div className="goal-name">
-            <label htmlFor='goal_name'>Goal Name</label>
+            <label htmlFor='goal_name'>Name</label>
             <input type="text" id='goal_name' {...register("goal_name")}/>
         </div>  
 
         <div className="goal-desc">
-            <label htmlFor='goal_desc'>Goal Description</label>
-            <input type="text" id='goal_desc' {...register("goal_desc")}/>
+            <label htmlFor='goal_desc'>Description</label>
+            <textarea id='goal_desc' rows="5" {...register("goal_desc")}/>
+        </div>
+
+        <div className="goal-end-date">
+            <label htmlFor='goal_end_date'>End Date</label>
+            <input type="date" id='goal_end_date' {...register("goal_end_date")} min={0}/>
         </div>
 
         <div className="goal-end-amount">
-            <label htmlFor='goal_end_amount'>End Goal Amount</label>
+            <label htmlFor='goal_end_amount'>Goal Amount</label>
             <input type="number" id='goal_end_amount' {...register("goal_end_amount")} min={0}/>
         </div>
 
