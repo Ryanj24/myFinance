79912705937595d24335c97269e1a52699ac3a3b
Ryanj24 <57965902+Ryanj24@users.noreply.github.com>
2024-01-24 16:17:05 +0000

    Fix made to portfolio overview component to correctly display full logo of the portfolio provider instead of the icon

--- frontend/src/Components/PortfolioOverview/PortfolioOverview.jsx
+++ frontend/src/Components/PortfolioOverview/PortfolioOverview.jsx
@@ -4,7 +4,24 @@ import { useParams } from 'react-router-dom';
 import { Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { portfolioIcon } from '../../utilityFunctions/portfolioIcon';
-import { portfolioIcons } from '../PortfolioCard/PortfolioCard';
+
+import AJBell from '../../assets/portfolio-logos-full/AJBell-full-logo.svg'
+import Barclays from '../../assets/bank-logos-full/Barclays_full.svg'
+import Fidelity from '../../assets/portfolio-logos-full/Fidelity-full-logo.svg'
+import Freetrade from '../../assets/portfolio-logos-full/Freetrade-full-logo.svg'
+import HSBC from '../../assets/bank-logos-full/HSBC_full.svg'
+import Lloyds from '../../assets/bank-logos-full/Lloyds_Bank_full.svg'
+import HL from '../../assets/portfolio-logos-full/HargreavesLansdown-full-logo.svg'
+import JPMorgan from '../../assets/portfolio-logos-full/JP-Morgan-full-logo.svg'
+import MorganStanley from '../../assets/portfolio-logos-full/Morgan_Stanley_Logo_1.svg'
+import NatWest from '../../assets/bank-logos-full/NatWest.svg'
+import RBS from '../../assets/bank-logo-icons/RBS_icon.svg'
+import Santander from '../../assets/bank-logos-full/Santander_full.svg'
+import Trading212 from '../../assets/portfolio-logos-full/Trading212-full-logo.svg'
+import Vanguard from '../../assets/portfolio-logos-full/Vanguard-full-logo.svg'
+import { AccountBalance } from '@mui/icons-material'
+
+const portfolioFullLogos = [AJBell, Barclays, Fidelity, Freetrade, HL, HSBC, Lloyds, JPMorgan, MorganStanley, NatWest, RBS, Santander, Trading212, Vanguard, <AccountBalance />]
 
 const PortfolioOverview = () => {
     const {id} = useParams();
@@ -18,15 +35,15 @@ const PortfolioOverview = () => {
     }
 
   return (
-    <section className='details-section'>
+    <section className='portfolio-details-section'>
         <div className="provider-logo-container">
             {portfolio && portfolio.iconIndex === 14
               ?
-                <>{portfolioIcons[portfolio.iconIndex]}</>
+                <>{portfolioFullLogos[portfolio.iconIndex]}</>
               : 
               (portfolio && portfolio.iconIndex !== 14)
               ?
-                <img src={portfolioIcons[portfolio.iconIndex]} alt={portfolio.provider + " Logo"} height="100%" width="100%"/>
+                <img src={portfolioFullLogos[portfolio.iconIndex]} alt={portfolio.provider + " Logo"} height="100%" width="100%"/>
               :
                 null
             }
